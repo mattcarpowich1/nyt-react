9b00397d84b39ab1c7f8680fecd528457e881c1b
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2017-12-08 14:58:12 -0800

    fixed indentation for src configuration files

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,32 +5,32 @@ const BUILD_DIR = path.resolve(__dirname, 'src/public');
 const APP_DIR = path.resolve(__dirname, 'src');
 
 const config = {
-	entry: APP_DIR + '/app/index.js',
-	output: {
-		path: BUILD_DIR,
-		filename: 'bundle.js'
-	},
-	resolve: {
-		modules: [__dirname, 'node_modules'],
-		extensions: ['*', '.js', '.css']
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.jsx?/,
-				include: APP_DIR,
-				loader: 'babel-loader'
-			},
-			{
-				test: /\.css$/,
-				loader: "style-loader!css-loader"
-			},
-			{
+  entry: APP_DIR + '/app/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    modules: [__dirname, 'node_modules'],
+    extensions: ['*', '.js', '.css']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
         test: /\.(jpe?g|png|gif|svg)$/i, 
         loader: "file-loader?name=/public/images/[name].[ext]"
       }
-		]
-	}
+    ]
+  }
 }
 
 module.exports = config;
