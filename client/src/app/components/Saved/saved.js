bba744b2dfd252107f08efa2b70ffaedf6ac7838
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2017-12-08 15:17:42 -0800

    fixed indentation on saved component

--- client/src/app/components/Saved/saved.js
+++ client/src/app/components/Saved/saved.js
@@ -4,65 +4,65 @@ import './saved.css'
 
 class Saved extends Component {
 
-	state = {
-		articles: []
-	}
+  state = {
+    articles: []
+  }
 
-	componentDidMount() {
-		API.findAll()
-			.then(response => {
-				this.setState({ articles: response.data})
-			})
-			.catch(err => {
-				console.log(err)
-			})
-	}
+  componentDidMount() {
+    API.findAll()
+      .then(response => {
+        this.setState({ articles: response.data})
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
-	handleDelete(_id) {
-		API.deleteArticle(_id)
-			.then(article => {
-				API.findAll()
-					.then(response => {
-						this.setState({ articles: response.data })
-					})
-					.catch(err => {
-						console.log(err)
-					})
-			})
-			.catch(err => {
-				console.log(err)
-			})
-	}
+  handleDelete(_id) {
+    API.deleteArticle(_id)
+      .then(article => {
+        API.findAll()
+          .then(response => {
+            this.setState({ articles: response.data })
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
-	render() {
-		return (
-			<div className="saved">
-				<h2>Saved Articles</h2>
-				{
-					this.state.articles.map(article => {
-						return (
-							<div className="card result" key={article._id}>
-								<header className="card-header">
-									<h3 className="card-header-title">
-										{ article.headline }
-									</h3>
-									<button className="button card-header-icon is-danger save"
-										onClick={ ()=> {
-											this.handleDelete(article._id)
-										}}>Delete</button>
-								</header>
-								<footer className="card-footer">
-									<p className="card-footer-item">
-										<a href={article.url}>Read Article</a>
-									</p>
-								</footer>
-							</div>
-						)
-					})
-				}
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="saved">
+        <h2>Saved Articles</h2>
+        {
+          this.state.articles.map(article => {
+            return (
+              <div className="card result" key={article._id}>
+                <header className="card-header">
+                  <h3 className="card-header-title">
+                    { article.headline }
+                  </h3>
+                  <button className="button card-header-icon is-danger save"
+                    onClick={ ()=> {
+                      this.handleDelete(article._id)
+                    }}>Delete</button>
+                </header>
+                <footer className="card-footer">
+                  <p className="card-footer-item">
+                    <a href={article.url}>Read Article</a>
+                  </p>
+                </footer>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
 	
 }
 
